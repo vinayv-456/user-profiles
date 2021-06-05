9a7abff52fe2dd85cf23a7aebe6a0baf496379fd
vinayv-456 <vinayvallabineni456@gmail.com>
2021-06-05 14:07:56 +0530

    Add loader and handle page break on reload in Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,36 +7,35 @@ import Profile from './Profile/Profile';
 import Chat from './Chats/chat'
 import { useEffect, useState } from "react";
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Home = (props) => {
     const [users, setUsers] = useState(props.users);
     const [path, setPath] = useState();
+    const [user, setUser] = useState({});
+    
+    useEffect(()=>{
+        setUsers(props.users);
+    }, [props.users])
 
     const loadUserDetails = (id) => {
-        console.log(users)
         if (users.length > 0) {
             const temp = users.filter((user) => user.id == id)
-            console.log("tmep", temp);
-            return temp;
+            setUser(temp[0]);
         }
     }
 
     useEffect(() => {
         const temp = props.location.pathname.split('/');
         setPath(temp[temp.length - 1]);
-        console.log("path", path);
     }, [props.location.pathname])
 
-    const [user, setUser] = useState(loadUserDetails(props.match.params.id)[0]);
-
     useEffect(() => {
-        setUser(loadUserDetails(props.match.params.id)[0])
-    }, [props.match.params.id])
+            loadUserDetails(props.match.params.id);
+    }, [props.match.params.id, users])
 
     return <div style={{ display: 'flex', padding: '35px', position: 'relative' }}>
         <div className="menu-container">
-            {/* <ul> */}
-            {console.log("props?????????", props)}
             <div style={{ display: 'flex', flexDirection: 'column', color: 'white', width: '100%', margin: '20px' }}>
                 <Link className="link" to={`${props.match.url}/profile`}>
                     <div className={(path === "profile" || path === '') ? "active-link" : "links"}>
@@ -94,39 +93,43 @@ const Home = (props) => {
         </div>
         <div style={{ width: '80vw', height: '100vh', marginLeft: '30px', marginRight: '30px'}}>
             <Header {...props} user={user} users={users} />
-            <Switch>
-                {/* <Route path="/"
-                        <Redirect from="/home/:id" to="/home/:id/posts"/> 
-                    render={(props)=>(
-                        <Header heading="Posts"/>
-                    )}
-                /> */}
-                <Route path={`/home/:id/posts`}
-                    render={(props) => (
-                        <CommingSoon {...props} />
-                    )}
-                />
-                <Route path={`/home/:id/gallery`}
-                    render={(props) => (
-                        <CommingSoon {...props} />
-                    )}
-                />
-                <Route path={`/home/:id/todo`}
-                    render={(props) => (
-                        <CommingSoon {...props} />
-                    )}
-                />
-                <Route path={`/home/:id/`}
-                    render={(props) => (
-                        <Profile {...props} user={user} />
-                    )}
-                />
-            </Switch>
-            <Route path={`/home/:id/`}
-                render={(props) => (
-                    <Chat {...props} user={user} users={users} />
-                )}
-            />
+            {
+                Object.keys(user).length === 0
+                ?
+                <div className="center">
+                    <CircularProgress />
+                </div>
+                :
+                <div>
+                    <Switch>
+                        <Route path={`/home/:id/posts`}
+                            render={(props) => (
+                                <CommingSoon {...props} />
+                            )}
+                        />
+                        <Route path={`/home/:id/gallery`}
+                            render={(props) => (
+                                <CommingSoon {...props} />
+                            )}
+                        />
+                        <Route path={`/home/:id/todo`}
+                            render={(props) => (
+                                <CommingSoon {...props} />
+                            )}
+                        />
+                        <Route path={`/home/:id/`}
+                            render={(props) => (
+                                <Profile {...props} user={user} />
+                            )}
+                        />
+                    </Switch>
+                    <Route path={`/home/:id/`}
+                        render={(props) => (
+                            <Chat {...props} user={user} users={users} />
+                        )}
+                    />
+                </div>
+            }
         </div>
     </div>
 }
